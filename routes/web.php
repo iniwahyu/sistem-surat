87d66cf0a4bedd4297acdc4faa908d9ff6ea185a
Wahyu Rizky <wrep17@gmail.com>
2022-01-20 23:50:38 +0700

    Add: Route Resource for master/role

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/**
- * MASTER
- */
-// Role
-Route::resource('/master/role', 'Master\Role');
-
 Route::get('/', function () {
     return view('welcome');
 });
